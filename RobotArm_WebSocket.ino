8c335ef6b56ae9ab621cef82ca6865354401baec
Matheus de Novais Souza <108086621+MatheusNovaisOV@users.noreply.github.com>
2023-05-21 00:33:30 -0300

    server flask_socketio / change arduino code

--- RobotArm_WebSocket.ino
+++ RobotArm_WebSocket.ino
@@ -1,102 +1,116 @@
-#include <SPI.h>
-#include <Ethernet.h>
+#include <Ethernet.h> //Load Ethernet Library
+#include <EthernetUdp.h> //Load the Udp Library
+#include <SPI.h> //Load SPI Library
+#include <Servo.h>
 
-// Insira abaixo um endereço MAC e um endereço IP para o seu controlador.
-// O endereço IP dependerá da sua rede local:
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
-IPAddress ip(192, 168, 0, 177);
+#include "Wire.h" //imports the wire library
 
-// Inicializa a biblioteca do servidor Ethernet
-// com o endereço IP e a porta desejada
-// (a porta 80 é a padrão para HTTP):
-EthernetServer server(80);
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE}; //Assign mac address
+IPAddress ip(192, 168, 0, 243); //Assign the IP Adress
+unsigned int localPort = 80; // Assign a port to talk over
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //dimensian a char array to hold our data packet
+String datReq; //String for our data
+int packetSize; //Size of the packet
+EthernetUDP Udp; // Create a UDP Object
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
 
 void setup() {
-  // Você pode usar Ethernet.init(pino) para configurar o pino CS
-  Ethernet.init(10); // A maioria dos shields Arduino
-  //Ethernet.init(5); // Shield MKR ETH
-  //Ethernet.init(0); // Teensy 2.0
-  //Ethernet.init(20); // Teensy++ 2.0
-  //Ethernet.init(15); // ESP8266 com Adafruit Featherwing Ethernet
-  //Ethernet.init(33); // ESP32 com Adafruit Featherwing Ethernet
+  servo1.attach(9);
+  servo2.attach(11);
+  servo3.attach(8);
+  servo4.attach(10);
 
-  // Abre a comunicação serial e aguarda a abertura da porta:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // aguarda a conexão com a porta serial. Necessário apenas para a porta USB nativa
-  }
-  Serial.println("Exemplo de Servidor Web Ethernet");
+  /*base.attach(8);
+  braco.attach(9);
+  garra.attach(11);
+  cotovelo.attach(10);*/
 
-  // Inicia a conexão Ethernet e o servidor:
-  Ethernet.begin(mac, ip);
+  Serial.begin(9600); //Initialize Serial Port
+  Ethernet.begin( mac, ip); //Inialize the Ethernet
+  Udp.begin(localPort); //Initialize Udp
+  delay(1500); //delay
 
-  // Verifica se há hardware Ethernet presente
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Shield Ethernet não encontrado. Desculpe, não é possível executar sem o hardware. :(");
-    while (true) {
-      delay(1); // não faz nada, não há motivo para continuar sem o hardware Ethernet
-    }
-  }
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("O cabo Ethernet não está conectado.");
-  }
-
-  // Inicia o servidor
-  server.begin();
-  Serial.print("O servidor está em ");
-  Serial.println(Ethernet.localIP());
 }
 
 void loop() {
-  // aguarda por clientes conectados
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println("Novo cliente");
-    // uma requisição http termina com uma linha em branco
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // se você chegou ao final da linha (recebeu uma quebra de linha
-        //) e a linha está em branco, a requisição http terminou,
-        // então você pode enviar uma resposta
-        if (c == '\n' && currentLineIsBlank) {
-          // envia um cabeçalho de resposta http padrão
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close"); // a conexão será fechada após a conclusão da resposta
-          client.println("Refresh: 5"); // atualiza a página automaticamente a cada 5 segundos
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // imprime o valor de cada pino de entrada analógica
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("Entrada analógica ");
-            client.print(analogChannel);
-            client.print(" é ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // você está começando uma nova linha
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // você recebeu um caractere na linha atual
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // dá ao navegador tempo para receber os dados
-    delay(1);
-    // fecha a conexão:
-    client.stop();
-    Serial.println("Cliente desconectado");
+
+  packetSize = Udp.parsePacket(); //Reads the packet size
+
+  if (packetSize > 0) { //if packetSize is >0, that means someone has sent a request
+
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE); //Read the data request
+    String datReq(packetBuffer); //Convert char array packetBuffer into a string called datReq
+
+    Serial.println("datReq: " + datReq);
+    // Processa o comando HTTP do servidor Flask
+    processRequest(datReq);
+
   }
+  memset(packetBuffer, 0, UDP_TX_PACKET_MAX_SIZE); //clear out the packetBuffer array
+}
+
+void processRequest(String request) {
+  // Extrai o ID do servo e o ângulo da solicitação
+  int servoId = extractServoId(request);
+  int angle = extractAngle(request);
+
+  Serial.println("angle: " + String(angle));
+  Serial.println("servoId: " + String(servoId));
+
+  // Move o servo conforme necessário
+  switch (servoId) {
+    case 1:
+      moveServo(servo1, angle);
+      break;
+    case 2:
+      moveServo(servo2, angle);
+      break;
+    case 3:
+      moveServo(servo3, angle);
+      break;
+    case 4:
+      moveServo(servo4, angle);
+      break;
+    default:
+      break;
+  }
+}
+
+int extractServoId(String request) {
+  int idIndex = request.indexOf("servo_id=");
+  if (idIndex != -1) {
+    int endIndex = request.indexOf('&', idIndex);
+    if (endIndex != -1) {
+      String idString = request.substring(idIndex + 9, endIndex);
+      return idString.toInt();
+    }
+  }
+  return -1;  // Retornar -1 se não encontrar o ID do servo na solicitação
+}
+
+int extractAngle(String request) {
+  int angleIndex = request.indexOf("angle=");
+  if (angleIndex != -1) {
+    int endIndex = request.indexOf(' ', angleIndex);
+    if (endIndex != -1) {
+      String angleString = request.substring(angleIndex + 6, endIndex);
+      return angleString.toInt();
+    }
+  }
+  return -1;  // Retornar -1 se não encontrar o ângulo na solicitação
+}
+
+void moveServo(Servo servo, int angle) {
+  // Limita o ângulo entre 0 e 180
+  angle = constrain(angle, 0, 180);
+
+  // Move o servo para o ângulo desejado
+  servo.write(angle);
+
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort()); //Initialize packet send
+  Udp.print(angle); //Send the temperature data
+  Udp.endPacket(); //End the packet
 }
---
+++ flask_socket.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, send
+from socket import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
+socketio = SocketIO(app)
+
+client_socket =socket(AF_INET, SOCK_DGRAM) #Set up the Socket
+
+clients = []
+address = ('192.168.0.243', 80)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print("client connected")
+    clients.append(request.namespace)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("client disconnected")
+    clients.remove(request.namespace)
+
+@socketio.on('update_servo')
+def handle_update_servo(data):
+    servo_id = data['servoId']
+    angle = data['angle']
+
+    print('servo_id: '+str(servo_id)+' angle: '+str(angle))
+
+    resp = 'servo_id='+str(servo_id)+'&angle='+str(angle)+' '
+
+    client_socket.sendto( resp.encode(), address)
+
+if __name__ == '__main__':
+    socketio.run(app)
