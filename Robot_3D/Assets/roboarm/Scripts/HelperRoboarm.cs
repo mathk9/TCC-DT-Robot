138702e3fb86be5637c15311ae19ce9214a5288e
YsabelaMolero <ysabelamoleror@gmail.com>
2023-11-02 15:20:00 -0300

    Feat: Adicionado files do modelo 3D

---
+++ Robot_3D/Assets/roboarm/Scripts/HelperRoboarm.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelperRoboarm : MonoBehaviour
+{
+    private const string SERVO_REFERENCES_PATH = "servo_references";
+    private const string SERVO_1_REF_PATH = SERVO_REFERENCES_PATH + "/ref_servo_1";
+    private const string SERVO_2_REF_PATH = SERVO_REFERENCES_PATH + "/ref_servo_2";
+    private const string SERVO_3_REF_PATH = SERVO_REFERENCES_PATH + "/ref_servo_3";
+    private const string SERVO_4_REF_PATH = SERVO_REFERENCES_PATH + "/ref_servo_4";
+    private const string SERVO_1_REF_VECTOR_PATH = SERVO_1_REF_PATH + "/ref_servo_1_vector";
+    private const string SERVO_2_REF_VECTOR_PATH = SERVO_2_REF_PATH + "/ref_servo_2_vector";
+    private const string SERVO_3_REF_VECTOR_PATH = SERVO_3_REF_PATH + "/ref_servo_3_vector";
+    private const string SERVO_4_REF_VECTOR_PATH = SERVO_4_REF_PATH + "/ref_servo_4_vector";
+    private const string SERVO_1_REF_ROTATION_PATH = SERVO_1_REF_VECTOR_PATH + "/ref_servo_1_rotation";
+    private const string SERVO_2_REF_ROTATION_PATH = SERVO_2_REF_VECTOR_PATH + "/ref_servo_2_rotation";
+    private const string SERVO_3_REF_ROTATION_PATH = SERVO_3_REF_VECTOR_PATH + "/ref_servo_3_rotation";
+    private const string SERVO_4_REF_ROTATION_PATH = SERVO_4_REF_VECTOR_PATH + "/ref_servo_4_rotation";
+
+
+    
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Robot_3D/Assets/roboarm/Scripts/MqttManager.cs
@@ -0,0 +1,142 @@
+using UnityEngine;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+using PimDeWitte.UnityMainThreadDispatcher;
+
+public class MqttManager : MonoBehaviour
+{
+    public static MqttManager instancia;
+
+    private void Awake() {
+        instancia = this;
+    }
+
+    private MqttClient client;
+
+    public string brokerIpAddress = "52.149.214.24";
+    public int brokerPort = 1883;
+    public string clientId = "UnityClient";
+    public string subscribeTopic = "/TEF/DeviceRoboArm001/cmd";
+    public string publishTopic = "/TEF/DeviceRoboArm001/attrs";
+
+    private void Start()
+    {
+        client = new MqttClient(brokerIpAddress);
+        client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+        string clientId = "UnityClient_" + System.Guid.NewGuid().ToString();
+        client.Connect(clientId);
+
+        if (client.IsConnected)
+        {
+            client.Subscribe(new string[] { subscribeTopic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            Debug.Log("Connect to MQTT broker.");
+        }
+        else
+        {
+            Debug.LogError("Failed to connect to MQTT broker.");
+        }
+    }
+
+    private void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        string jsonString = Encoding.UTF8.GetString(e.Message);
+
+        Debug.Log("jsonString => "+jsonString);
+
+        RootObject obj = JsonUtility.FromJson<RootObject>(jsonString);
+        
+        // A partir deste ponto, você pode acessar os dados desserializados usando obj.subscriptionId, obj.data, etc.
+        
+        // Determinando qual motor foi enviado e seu valor
+        if (obj.data != null && obj.data.Length > 0)
+        {
+            UnityMainThreadDispatcher.Instance().Enqueue(() => {
+                // Operações que na thread principal
+                Data motorData = obj.data[0];
+                if (!float.IsNaN(motorData.motor1))
+                {
+                    Debug.Log("Motor 1 foi enviado com valor: " + motorData.motor1);
+                    Roboarm.instancia.SetAngleRotationServo1(motorData.motor1);
+                }
+                else if (!float.IsNaN(motorData.motor2))
+                {
+                    Debug.Log("Motor 2 foi enviado com valor: " + motorData.motor2);
+                    Roboarm.instancia.SetAngleRotationServo2(motorData.motor2);
+                }
+                else if (!float.IsNaN(motorData.motor3))
+                {
+                    Debug.Log("Motor 3 foi enviado com valor: " + motorData.motor3);
+                    Roboarm.instancia.SetAngleRotationServo3(motorData.motor3);
+                }
+                else if (!float.IsNaN(motorData.motor4))
+                {
+                    Debug.Log("Motor 4 foi enviado com valor: " + motorData.motor4);
+                    Roboarm.instancia.SetAngleRotationServo4(motorData.motor4);
+                }
+                else
+                {
+                    Debug.Log("Nenhum motor foi enviado no JSON.");
+                }
+            });
+        }
+        else
+        {
+            Debug.Log("Nenhum dado foi recebido no JSON.");
+        }       
+    }
+
+    public void MoveRoboArmServo1(float motor1Angle)
+    {     
+        client.Publish(publishTopic, Encoding.UTF8.GetBytes("ld|VirtualRoboArm"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+        client.Publish(publishTopic, Encoding.UTF8.GetBytes($"mt1|{motor1Angle}"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+    }
+
+    public void MoveRoboArmServo2(float motor2Angle)
+    {     
+        client.Publish(publishTopic, Encoding.UTF8.GetBytes("ld|VirtualRoboArm"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+        client.Publish(publishTopic, Encoding.UTF8.GetBytes($"mt2|{motor2Angle}"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+    }
+
+    public void MoveRoboArmServo3(float motor3Angle)
+    {     
+        client.Publish(publishTopic, Encoding.UTF8.GetBytes("ld|VirtualRoboArm"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+        client.Publish(publishTopic, Encoding.UTF8.GetBytes($"mt3|{motor3Angle}"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+    }
+
+    public void MoveRoboArmServo4(float motor4Angle)
+    {     
+        client.Publish(publishTopic, Encoding.UTF8.GetBytes("ld|VirtualRoboArm"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+        client.Publish(publishTopic, Encoding.UTF8.GetBytes($"mt4|{motor4Angle}"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+    }
+    
+    private void OnDestroy()
+    {
+        if (client != null && client.IsConnected)
+        {
+            client.Disconnect();
+        }
+    }
+
+    [System.Serializable]
+    public class Data
+    {
+        public string id;
+        public string type;
+        public float motor1 = float.NaN;
+        public float motor2 = float.NaN;
+        public float motor3 = float.NaN;
+        public float motor4 = float.NaN;
+        public string lastDevice;
+    }
+
+    [System.Serializable]
+    public class RootObject
+    {
+        public string subscriptionId;
+        public Data[] data;
+    }
+}
